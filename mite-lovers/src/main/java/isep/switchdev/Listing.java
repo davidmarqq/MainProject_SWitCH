c64bfc86dff58d6954484f6244d9a37bfb66eb79
Ines Giesteira <1251997@isep.ipp.pt>
2025-12-14 18:47:39 +0000

    Fixed code

--- mite-lovers/src/main/java/isep/switchdev/Listing.java
+++ mite-lovers/src/main/java/isep/switchdev/Listing.java
@@ -3,20 +3,38 @@ package isep.switchdev;
 public class Listing {
 
 
-    private Item item;
-    private double price;
-    private String shortDescription;
-    private String listingTitle;
-    private SaleType saleType;
-    private User seller;
+    private Item _item;
+    private double _price;
+    private String _shortDescription;
+    private String _listingTitle;
+    private SaleType _saleType;
+    private User _seller;
 
     public Listing(Item item, double price, String shortDescription, String listingTitle, SaleType saleType, User seller) {
-        this.item = item;
-        this.price = price;
-        this.shortDescription = shortDescription;
-        this.listingTitle = listingTitle;
-        this.saleType = saleType;
-        this.seller = seller;
-    }
 
+        if (item == null) {
+            throw new IllegalArgumentException("Item cannot be null.");
+        }
+
+        if (listingTitle == null ||  listingTitle.isBlank()) {
+            throw new IllegalArgumentException("Listing title cannot be null or blank.");
+        }
+        if (saleType == null) {
+            throw new IllegalArgumentException("Sale type cannot be null.");
+        }
+        if (seller == null) {
+            throw new IllegalArgumentException("User cannot be null.");
+        }
+
+        if (price <= 0) {
+            throw new IllegalArgumentException("Price must be greater than 0.");
+        }
+
+        this._item = item;
+        this._price = price;
+        this._shortDescription = shortDescription;
+        this._listingTitle = listingTitle;
+        this._saleType = saleType;
+        this._seller = seller;
+    }
 }

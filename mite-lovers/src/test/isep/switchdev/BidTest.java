ecd0f0eac62b252c1bcc36ac7ac6c3aca38e5704
Helena Matos <1251992@isep.ipp.pt>
2025-12-14 23:25:19 +0000

    Initialized Bid class

--- mite-lovers/src/test/isep/switchdev/BidTest.java
+++ mite-lovers/src/test/isep/switchdev/BidTest.java
@@ -1,7 +1,16 @@
 package isep.switchdev;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BidTest {
+    @Test
+    void construtorInicialized() {
+        User Bid = new Bid ("Maria", 20);
 
+        assertEquals("Maria", Bid.getBidder());
+        assertEquals(20, Bid.getvalue());
+
+    }
 }

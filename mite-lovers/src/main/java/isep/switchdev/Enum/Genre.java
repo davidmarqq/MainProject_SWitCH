8ec69249ea877a8b34d8b98e98ba80a4d51cb303
rodrigo_diogo <1252006@isep.ipp.pt>
2025-12-15 13:58:48 +0000

    Created genre

--- mite-lovers/src/main/java/isep/switchdev/Enum/Genre.java
+++ mite-lovers/src/main/java/isep/switchdev/Enum/Genre.java
@@ -1,4 +1,12 @@
 package isep.switchdev.Enum;
 
 public enum Genre {
+    FICTION,
+    BIBLIOGRAPHICAL,
+    ROMANCE,
+    FANTASY,
+    NONFICTION,
+    ADVENTURE,
+    MYSTERY,
+    POETRY
 }
--- mite-lovers/src/main/java/isep/switchdev/Publisher.java
+++ mite-lovers/src/main/java/isep/switchdev/Publisher.java
@@ -1,4 +1,34 @@
 package isep.switchdev;
 
 public class Publisher {
+
+    private String name;
+    private String vatNumber;
+    private Address address;
+    private Contact contact;
+
+    public Publisher(String name, String vatNumber, Address address, Contact contact) {
+        this.name = name;
+        this.vatNumber = vatNumber;
+        this.address = address;
+        this.contact = contact;
+    }
+
+    // Review what methods we will want in each class
+
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public String getVatNumber() {
+//        return vatNumber;
+//    }
+//
+//    public Address getAddress() {
+//        return address;
+//    }
+//
+//    public Contact getContact() {
+//        return contact;
+//    }
 }

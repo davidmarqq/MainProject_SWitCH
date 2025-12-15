68b15e2fff68cf23734e971651391d4c233b6efa
Helena Matos <1251992@isep.ipp.pt>
2025-12-15 11:54:10 +0000

    Updated Bid.class and BidTest.class

--- mite-lovers/src/main/java/isep/switchdev/Bid.java
+++ mite-lovers/src/main/java/isep/switchdev/Bid.java
@@ -3,10 +3,26 @@ package isep.switchdev;
 public class Bid {
     private User bidder;
     private double value;
+    // private Auction auction;  // Referência ao leilão
+
 
     public Bid (User bidder, double value) {
+
+
+        //Considered that the bidder can't be null
+        if (bidder == null) {
+            throw new IllegalArgumentException("bidder can't be null.");
+        }
+
+        // Value cannot be negative
+        if (value <= 0) {
+            throw new IllegalArgumentException("Bid value must be positive.");
+        }
+
         this.bidder = bidder;
         this.value = value;
+        // this.auction = auction;
+
 
     }
 
@@ -17,4 +33,17 @@ public class Bid {
     public double getValue(){
         return value;
     }
+
+    //  public Auction getAuction() {
+    //        return auction;
+    //    }
+
+    public String getBidInfo() {
+        return "Bidder: " + bidder.getUserName() + ", Value: " + value;
+    }
+
+
+
+    //Do I put a conncetion to Auction??
+
 }

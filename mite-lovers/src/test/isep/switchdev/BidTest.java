f90cdd57b643af5089102aff194c7ac57c0d1c6f
Helena Matos <1251992@isep.ipp.pt>
2025-12-15 12:11:02 +0000

    Updated BidTest.class

--- mite-lovers/src/test/isep/switchdev/BidTest.java
+++ mite-lovers/src/test/isep/switchdev/BidTest.java
@@ -5,6 +5,24 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BidTest {
+
+    //Test: Constructor&getFullAddress
+    @Test
+    void constructorShouldCreateBidWhenValid() {
+        //Arrange
+        Bid User = "Maria";
+        double value = 100.50;
+
+
+        //Act
+        Bid result = new Bid(User, value);
+        Bid expected = new Bid("Maria", 100.50 );
+
+        //Assert
+        assertEquals(result.getBidder(), expected.getValue());
+    }
+
+
     @Test
     void construtorInicialized() {
         Bid User = new Bid("Maria");

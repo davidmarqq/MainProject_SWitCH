3abba9e1b612eae9077b8e585699d63767644087
Bruno <1251985@isep.ipp.pt>
2025-12-15 14:37:54 +0000

    Add Code entity aligned with domain rules

---
+++ mite-lovers/src/main/java/isep/switchdev/Code.java
@@ -0,0 +1,23 @@
+package isep.switchdev;
+
+public class Code {
+    private final String isbn10;
+    private final String isbn13;
+    private final String issn;
+
+    public Code(String isbn10, String isbn13, String issn) {
+
+        this.isbn10 = isbn10;
+        this.isbn13 = isbn13;
+        this.issn = issn;
+    }
+    public String getIsbn10() {
+        return isbn10;
+    }
+    public String getIsbn13() {
+        return isbn13;
+    }
+    public String getIssn() {
+        return issn;
+    }
+}

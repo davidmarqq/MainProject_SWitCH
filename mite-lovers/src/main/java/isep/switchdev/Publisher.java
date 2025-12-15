0a67ab5d0296a772116e743a2b9651af96c766cf
rodrigo_diogo <1252006@isep.ipp.pt>
2025-12-15 14:28:24 +0000

    Finished Publisher for now

--- mite-lovers/src/main/java/isep/switchdev/Publisher.java
+++ mite-lovers/src/main/java/isep/switchdev/Publisher.java
@@ -4,13 +4,11 @@ public class Publisher {
 
     private String name;
     private String vatNumber;
-    private Address address;
     private Contact contact;
 
     public Publisher(String name, String vatNumber, Address address, Contact contact) {
         this.name = name;
         this.vatNumber = vatNumber;
-        this.address = address;
         this.contact = contact;
     }
 
